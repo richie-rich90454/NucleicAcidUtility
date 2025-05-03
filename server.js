59e6dcd867b2e1e393d5aa024467fa18c52e0b2d
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-05-03 13:51:06 +0800

    converted server.js to fastify

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express=require("express");
-const path=require("path");
-const app=express();
-const PORT=6001;
-app.use(express.static(__dirname));
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "index.html"));
+let fastify=require("fastify")({logger: false});
+let PORT=6001;
+fastify.register(require("@fastify/static"),{
+    root: __dirname,
+    prefix: "/",
+    setHeaders: (res, filePath)=>{
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.setHeader("Pragma", "no-cache");
+        res.setHeader("Expires", "0");
+    }
 });
-app.listen(PORT, ()=>{
+fastify.get("/", (request, reply)=>{
+    return reply.sendFile("index.html");
+});
+fastify.listen({port: PORT, host: "::", keepAliveTimeout: 5000, connectionTimeout: 5000},(err)=>{
+    if (err){
+        console.error(err);
+        process.exit(1);
+    }
     console.log(`Server running at http://localhost:${PORT}`);
 });
